bc91d43e058ccc95c05b20b69d7291489e1c9716
jacoboglez <jacobo.baldonedo@gmail.com>
2021-05-26 13:22:04 +0200

    2015 day 8 added.

---
+++ 2015/08.py
@@ -0,0 +1,18 @@
+
+def part1(inputlst):
+    memory = 0
+    for inputstr in inputlst:
+        # print(inputstr[:-1], ' -> ', eval(inputstr))
+        memory += len(inputstr[:-1])
+        memory -= len(eval(inputstr))
+    
+    return memory
+
+
+def part2(inputlst):    
+    return sum( s.count('"')+s.count("\\")+2  for s in inputlst )
+
+
+if __name__ == "__main__":
+    print(f"Part 1: {part1( open('2015/inputs/08.txt') )}")
+    print(f"Part 2: {part2( open('2015/inputs/08.txt') )}")
