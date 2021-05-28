680096f165ed6a4c842c54c05924d941f970b48e
jacoboglez <jacobo.baldonedo@gmail.com>
2021-05-28 13:58:51 +0200

    2015 day 12 (complete) added.

--- 2015/12.py
+++ 2015/12.py
@@ -1,4 +1,5 @@
 import re
+import json
 
 
 TESTS1 = [
@@ -13,10 +14,10 @@ TESTS1 = [
 ]
 
 TESTS2 = [
-('[1,2,3,-5,5]', 6),
-('[1,{"c":"red","b":2},3]', 4),
-('{"d":"red","e":[1,2,3,4],"f":5}', 0),
-('[1,"red",5]', 6),
+([1,2,3,-5,5], 6),
+([1,{"c":"red","b":2},3], 4),
+({"d":"red","e":[1,2,3,4],"f":5}, 0),
+([1,"red",5], 6),
 ]
 
 
@@ -25,8 +26,29 @@ def part1(inputstr):
     return sum( int(n) for n in g )
 
 
-def part2(inputstr):
-    pass
+def part2(file_name):
+    with open(file_name) as json_file:
+        data = json.load(json_file)
+
+    return crawler(data)
+
+
+def crawler(data):
+    acum = 0
+    if type(data) is dict:
+        if "red" in data.values():
+                return 0
+        data = data.values()
+
+    for el in data:
+        if type(el) is int:
+            acum += el
+        elif type(el) is list:
+            acum += crawler(el)
+        elif type(el) is dict:
+            acum += crawler(el)
+    
+    return acum
 
 
 def test():
@@ -34,9 +56,9 @@ def test():
         if part1(inp) != res:
             raise AssertionError
 
-    # for inp, res in TESTS2:
-    #     if part2(inp) != res:
-    #         raise AssertionError
+    for inp, res in TESTS2:
+        if crawler(inp) != res:
+            raise AssertionError
 
     print('Tests SUCCESSFUL')
 
@@ -45,4 +67,4 @@ if __name__ == "__main__":
     test() 
     inputlst = open('2015/inputs/12.txt', 'r').read().strip()
     print(f'Part 1: {part1(inputlst)}')
-    print(f'Part 2: {part2(inputlst)}')
+    print(f'Part 2: {part2("2015/inputs/12.txt")}')
